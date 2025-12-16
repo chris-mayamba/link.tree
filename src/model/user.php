bfb20c8f8415a8acedcb4410530abc19366047f9
chris-mayamba <christianmayamba04@gmail.com>
2025-12-16 13:45:50 +0200

    Add home and login view files

--- src/model/user.php
+++ src/model/user.php
@@ -1,29 +1,35 @@
 <?php
 
+global $users;
 $users = [
     [
-        "id" => 1,
-        "username" => "chris",
-        "password" => "abcd123"
+        'id' => 1,
+        'username' => 'chris',
+        'password' => 'abcd123'
     ],
-
     [
-        "id" => 2,
-        "username" => "eden",
-        "password" => "ab123"
+        'id' => 2,
+        'username' => 'eden',
+        'password' => 'ab123'
     ],
-
     [
-        "id" => 3,
-        "username" => "florette",
-        "password" => "ad123"
+        'id' => 3,
+        'username' => 'florette',
+        'password' => 'ad123'
     ],
-    
     [
-        "id" => 4,
-        "username" => "jahlife",
-        "password" => "abcd1235"
+        'id' => 4,
+        'username' => 'jahlife',
+        'password' => 'abcd1235'
     ]
 ];
 
-return $users;
+function find_user(string $username, string $password): ?array
+{
+    foreach ($users as $user) {
+        if ($user['username'] === $username && $user['password'] === $password) {
+            return $user;
+        }
+    }
+    return null;
+}
