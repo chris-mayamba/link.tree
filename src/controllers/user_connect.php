2861fce65b25ea8f3ca41ac9aec61638eb162575
chris-mayamba <christianmayamba04@gmail.com>
2026-01-29 17:06:28 +0200

    dashboard

--- src/controllers/user_connect.php
+++ src/controllers/user_connect.php
@@ -2,14 +2,22 @@
 
 require(__DIR__ . "/../model/user.php");
 
-try {
-    if (!isset($_POST["usernane"]) || !isset($_POST["password"])) {
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-        find_user($username, $password);
+if (isset($_POST["username"]) && isset($_POST["password"])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $user = find_user($username, $password);
 
+    if ($user) {
+        // Utilisateur trouvé, rediriger vers le dashboard
         header("Location: ../view/dashboard.php");
+        exit;
+    } else {
+        // Identifiants incorrects
+        header("Location: ../view/login.php?error=1");
+        exit;
     }
-} catch (Exception $e) {
-    throw new Exception("");
+} else {
+    // Champs manquants
+    header("Location: ../view/login.php?error=2");
+    exit;
 }
